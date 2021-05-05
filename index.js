fa95d540c6609e80430099ea9c6f4242b2173846
Srushti <srushti.raybhoge1999@gmail.com>
2021-05-06 00:19:10 +0530

    some changed

--- index.js
+++ index.js
@@ -6,11 +6,39 @@ const port = process.env.PORT;
 const routes = require("./routes");
 const path = require("path");
 console.log(__dirname);
-const buildPath = path.join(__dirname, "..", "/build");
+const buildPath = path.join(__dirname, "/client/build");
 app.use(express.static(buildPath));
+// ** MIDDLEWARE ** //
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:8080",
+  "https://young-falls-90605.herokuapp.com/",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable");
+      callback(null, true);
+    } else {
+      console.log("Origin rejected");
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+app.use(cors(corsOptions));
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "/build/index.html"));
