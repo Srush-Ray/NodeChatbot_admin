786569e8896ef4242da3e1a5a51a0c7991ae9030
Srushti <srushti.raybhoge1999@gmail.com>
2021-05-06 20:17:27 +0530

    edit left

--- index.js
+++ index.js
@@ -34,15 +34,15 @@ app.use(express.urlencoded());
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.resolve(__dirname, "client/build")));
   // Handle React routing, return all requests to React app
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
   });
   app.use("/api/all", routes.admin);
 } else {
   app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
   });
   app.use("/api/all", routes.admin);
 }
